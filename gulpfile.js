f9fe3fd9cc2ccc479e20618f60cb5f43c0c64fd1
frmtr <4moroyut@gmail.com>
2019-09-13 10:51:13 +0900

    update: add file

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp=require('gulp'),
+pug=require('gulp-pug'),
+sass=require('gulp-sass');
+
+gulp.task('compile-pug', () => {
+    return (
+        gulp
+            .src('./dev/pug/*.pug')
+            .pipe(pug())
+            .pipe(gulp.dest('./'))
+    );
+});
+
+gulp.task('compile-scss', () =>{
+	return(
+		gulp
+			.src('./dev/scss/style.scss', {
+				sourcemaps: true
+			})
+			.pipe(sass())
+			.pipe(gulp.dest('./build/css'))
+	);
+});
+
+
+gulp.task('default', gulp.series(gulp.parallel('compile-pug')));
